48268eade26acf8695bfb8dc75d54c9b8a37d616
aln1234 <albinlamichhane9@gmail.com>
2024-09-23 23:40:22 +0300

    added code related to streamlit

--- Toxic_comment/app.py
+++ Toxic_comment/app.py
@@ -2,3 +2,20 @@
 import streamlit as st
 import joblib
 import numpy as np
+
+# Load the vectorizer
+vec = joblib.load("model/vectorizer.pkl")
+
+
+#load models and 'r' values
+label_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+models = {}
+rs ={}
+
+for label in label_cols:
+    #load each model and its corresponding 'r' value
+    model,r = joblib.load(f'model/model_{label}.pkl')
+    models[label] = model
+    rs[label] = r
+    
+
