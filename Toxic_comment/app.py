813c1f6bce6248946aef5fc5e58d588019d7ecf5
aln1234 <albinlamichhane9@gmail.com>
2024-09-24 11:44:05 +0300

    added minor changes

--- Toxic_comment/app.py
+++ Toxic_comment/app.py
@@ -2,3 +2,10 @@
 import streamlit as st
 import joblib
 import numpy as np
+
+#load the vectorizer
+try:
+    vec = joblib.load("model/vectorizer.pkl")
+except FileNotFoundError:
+    st.error("Vectorizer file not found. Please ensure 'vectorizer.pkl' is available ")
+    st.stop()
